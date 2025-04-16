5790295e4a0bbecc732e4d5b6bce362e1608bb47
심현진 <hyunjin.dev143@gmail.com>
2025-04-16 19:25:37 +0900

    Feat: Select 사용해서 form 구현

---
+++ src/ch0409/AttendenceBook_ex.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AttendenceBook_ex() {
+  return (
+    <>
+      <div></div>
+      {/* 구현하기 */}
+    </>
+  );
+}
+
+export default AttendenceBook_ex;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Element_ex from "./ch0409/Element_ex";
+import NameForm from "./ch0409/NameForm_ex";
+import SignUp from "./ch0416/SignUp";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Element_ex />
+    <SignUp />
   </StrictMode>
 );
