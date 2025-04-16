5d085589c01a564dc1660b778c083a44f5f362ed
심현진 <hyunjin.dev143@gmail.com>
2025-04-16 20:14:25 +0900

    Feat: Shared State 적용해서 섭씨&화씨 변환 계산기 구현

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import Element_ex from "./ch0409/Element_ex";
 import NameForm from "./ch0409/NameForm_ex";
 import SignUp from "./ch0416/SignUp";
+import Calculator from "./ch0416/Calculator";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Element_ex />
-    <SignUp />
+    <Calculator />
   </StrictMode>
 );
